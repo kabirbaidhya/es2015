fd09d639d71f4b3f7b41e739ff512e070e4f293c
Kabir <kabirbaidhya@gmail.com>
2016-03-09 12:27:28 +0545

    String interpolation

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
+var customer = { name: "Foo" }
+var card = { amount: 7, product: "Bar", unitprice: 42 }
+var message = `Hello ${customer.name},
+want to buy ${card.amount} ${card.product} for
+a total of ${card.amount * card.unitprice} bucks?`;
 
-// ES6 way
-setTimeout(() => console.log('Hello from the ES6'), 1000);
+console.log(message);
 
-// ES5 way
-setTimeout(function() {
-     console.log('Hello from the ES5');
-}, 1000);
+// The ES5 way
+var message5 = "Hello " + customer.name + ",\n" +
+"want to buy " + card.amount + " " + card.product + " for\n" +
+"a total of " + (card.amount * card.unitprice) + " bucks?";
+
+console.log(message5);
+
+console.log(message === message5);
